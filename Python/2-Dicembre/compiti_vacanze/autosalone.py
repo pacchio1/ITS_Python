4bf878a848fbcc475b2b99858fbecf918d2d4c9a
Pacchiotto <marcopac.363.d@gmail.com>
2022-12-27 16:55:59 +0100

    Update autosalone.py

--- Python/2-Dicembre/compiti_vacanze/autosalone.py
+++ Python/2-Dicembre/compiti_vacanze/autosalone.py
@@ -13,6 +13,9 @@ Menu="""
     z)esci
 -------------------
 """
+acquirente=[]
+auto=[]
+autosalone=[]
 scelta=-1
 while scelta!=0:
     os.system("cls")
@@ -22,12 +25,33 @@ while scelta!=0:
     match scelta:
         case 'a':
             print("operazione",scelta)
+            marca=input("marca:")
+            cilindrata=int(input("cilindrata:"))
+            anno_imm=input("anno di immatricolazione:")
+            nome_acq=input("acquirente nome:")
+            cognome_acq=input("acquirente cognome:")
+            acquirente.append(nome_acq)
+            acquirente.append(cognome_acq)
+
+            auto.append(marca)
+            auto.append(cilindrata)
+            auto.append(anno_imm)
+            auto.append(acquirente)
+
+            autosalone.append(auto)
+            print(autosalone)
 
         case 'b':
-            print("operazione",scelta)
+            print("*"*10,"auto di cilindrata superiore a 1500 cc","*"*10)
+            for i in range(0,len(autosalone)-1):
+                #print(i)
+                if (autosalone[i][1])>1500:
+                  #print(str(autosalone[i][3]))
+                  acquirente=autosalone[i][3]
+                  print(acquirente[1])
 
         case 'c':
-            print("operazione",scelta)
+           print("*"*10,"numero totale di auto che sono state immatricolate in un anno richiesto","*"*10)
 
         case 'd':
             print("operazione",scelta)
