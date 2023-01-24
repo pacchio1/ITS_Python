9c5e865b3c6a173dbed88dac95f1cd96f8400672
Pacchiotti Marco <57013700+pacchio1@users.noreply.github.com>
2023-01-24 11:16:02 +0100

    Update CDMusicale.py

--- Python/3-Gennaio/CDMusicale.py
+++ Python/3-Gennaio/CDMusicale.py
@@ -1,8 +1,14 @@
 from CDMusicaleClass import CD
-def creaCD():
+pop1=rock3=jazz=0
+def creaCD(pop1,rock3,jazz):
     pop1=CD("pop1",65,2010,5,10.0)
     rock3=CD("Rock-3-",108,2007,25,12.5)
     jazz=CD("jazz-7-",48,1990,5,13.0)
+    prezzo=input("prezzo")
+    pop1.setPrezzo(prezzo)
+
+def aggPrezzo(ogg):
+    None
 def main():
     creaCD()
 
