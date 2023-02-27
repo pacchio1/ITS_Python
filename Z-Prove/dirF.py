130286f59decb7c07ad51869f851d6b8e0a49ed8
Pacchiotti Marco <57013700+pacchio1@users.noreply.github.com>
2023-02-27 14:44:10 +0100

    prove

--- Z-Prove/dirF.py
+++ Z-Prove/dirF.py
@@ -1,9 +1,9 @@
 import math
-#print(dir(math))
+# print(dir(math))
 import random
-#print(dir(random))
-#print(math.pi)
-#print(dir())
-#print(__name__)
+# print(dir(random))
+# print(math.pi)
+# print(dir())
+# print(__name__)
 help(math)
 input()
